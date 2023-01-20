7a18e780c8ffce8035ef459bbb5ccb564796c83c
takuya_kajiwara <fd.elt.119@gmail.com>
2023-01-20 20:50:52 +0900

    タスク登録機能実装

--- resources/ts/api/TaskAPI.ts
+++ resources/ts/api/TaskAPI.ts
@@ -7,10 +7,17 @@ const getTasks = async () => {
 };
 
 const updateDoneTask = async ({ id, is_done }: Task) => {
-    const { data } = await axios.patch<Task[]>(`/api/tasks/update-done/${id}`, {
+    const { data } = await axios.patch<Task>(`/api/tasks/update-done/${id}`, {
         is_done: !is_done,
     });
     return data;
 };
 
-export { getTasks, updateDoneTask };
+const createTask = async (title: string) => {
+    const { data } = await axios.post<Task>(`/api/tasks`, {
+        title: title,
+    });
+    return data;
+};
+
+export { getTasks, updateDoneTask, createTask };
--- resources/ts/pages/tasks/components/TaskInput.tsx
+++ resources/ts/pages/tasks/components/TaskInput.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useCreateTask } from "../../../queries/TaskQuery";
 
 const TaskInput = () => {
+    const [title, setTitle] = useState("");
+    const createTask = useCreateTask();
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        createTask.mutate(title);
+        setTitle("");
+    };
+
     return (
-        <form className="input-form">
+        <form className="input-form" onSubmit={handleSubmit}>
             <div className="inner">
                 <input
+                    value={title}
                     type="text"
                     className="input"
                     placeholder="TODOを入力してください。"
-                    defaultValue=""
+                    // defaultValue={title}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setTitle(e.target.value)
+                    }
                 />
                 <button className="btn is-primary">追加</button>
             </div>
